eee6a6b1499557ff197ef3d6fbb451d71fdb8646
esteban <aefigueroaq@uni.pe>
2020-01-15 13:54:48 -0500

    v 1.0.0

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Python
+ this is my first experience with Python. I've learned how to use variables and do some arithmetic operations.
+
+ ## Usage
+ To use my scripts, you only have to run the following command:
+ ...
+ python file_name.py
+ ...
+
+ ## Author
+ - **Esteban**
---
+++ variables.py
@@ -0,0 +1,38 @@
+#x=10
+#y=5
+#z=10.0
+#print(x,y)
+#frase ='hola'
+#print(type(x))
+#print(type(z))
+#print(type(frase))
+#print(x+y)
+#w= x+y+z
+#print(w)
+#print(x*y)
+#print(x*z)
+#print(z/y)
+#print((x*z)/20)
+#print(1+2)
+# operaciones aritmeticas, multiplicion *, división /, suma +, resta -, divisionn entera //
+#print(6/5)
+#print(6//5)
+#x=10%7  # residuo
+#print(x)
+#y=3//5
+#print('El resultado del residuo entre 10 y 7 es:', x)
+#print(f'El resultado entre 3//5 es: {y}')
+#x=10**3 # operacion potencia
+#print('El valor de la potencia de 10 en 3 es:',x)
+# recibir un parametro por teclado
+#variable = int(input('Ingrese un entero:'))
+#print(variable)
+#print(type(variable))
+#print('Vamos a calcular el área de un círculo')
+#r = int(input('ingrese el radio:' ))
+#A = (3.1416)*(r**2)
+#print('El area es:', A)
+import math
+r= float(input('Ingrese el radio del círculo: '))
+area = round(math.pi*r**2)
+print('El area del círculo es:',area)
